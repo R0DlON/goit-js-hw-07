f158b5f6063bb36136c42dc2efc8cc1a18262283
R0DlON <rodionprokopchenkoo@gmail.com>
2021-09-08 14:28:00 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -13,27 +13,38 @@ const images = [
   },
 ];
 
-// находим куда нужно поместить
-const needingPlace = document.querySelector("#gallery");
-console.log(needingPlace);
+const listImage = images
+  .map((image) => {
+    return `<li><img src="${image.url}" alt="${image.alt}"></li>`;
+  })
+  .join("");
 
-const makeLiImg = images.map((image) => {
-  const li = document.createElement("li");
+const gallery = document
+  .getElementById("gallery")
+  .insertAdjacentHTML("afterbegin", listImage);
 
-  const img = document.createElement("img");
-  img.url = image.url;
-  img.alt = image.alt;
+// мои попытки
+// // находим куда нужно поместить
+// const needingPlace = document.querySelector("#gallery");
+// console.log(needingPlace);
 
-  li.appendChild(img);
-  // через "appendChild" - работает
-  // return needingPlace.appendChild(li);
+// const makeLiImg = images.map((image) => {
+//   const li = document.createElement("li");
 
-  return needingPlace.insertAdjacentHTML("afterbegin", li);
-});
-// В этом случае подставляется url, alt только от первого <img>
-// needingPlace.insertAdjacentHTML(
-//   "afterbegin",
-//   '<li><img url="https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", alt="White and Black Long Fur Cat"</li>',
-//   '<li><img url="https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", alt="Orange and White Koi Fish Near Yellow Koi Fish"</li>',
-//   '<li><img url="https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", alt="Group of Horses Running"</li>'
-// );
+//   const img = document.createElement("img");
+//   img.url = image.url;
+//   img.alt = image.alt;
+
+//   li.appendChild(img);
+//   // через "appendChild" - работает
+//   // return needingPlace.appendChild(li);
+
+//   return needingPlace.insertAdjacentHTML("afterbegin", li);
+// });
+// // В этом случае подставляется url, alt только от первого <img>
+// // needingPlace.insertAdjacentHTML(
+// //   "afterbegin",
+// //   '<li><img url="https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", alt="White and Black Long Fur Cat"</li>',
+// //   '<li><img url="https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", alt="Orange and White Koi Fish Near Yellow Koi Fish"</li>',
+// //   '<li><img url="https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", alt="Group of Horses Running"</li>'
+// // );
