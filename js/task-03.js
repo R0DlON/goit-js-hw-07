df8fac089c60b068043d2725027f2fbb1e472370
R0DlON <rodionprokopchenkoo@gmail.com>
2021-09-08 14:15:29 +0300

    try to do 3
    try to do 3

--- js/task-03.js
+++ js/task-03.js
@@ -19,13 +19,21 @@ console.log(needingPlace);
 
 const makeLiImg = images.map((image) => {
   const li = document.createElement("li");
-  console.log(li);
+
   const img = document.createElement("img");
   img.url = image.url;
   img.alt = image.alt;
 
   li.appendChild(img);
-  // console.log(li.children);
+  // через "appendChild" - работает
+  // return needingPlace.appendChild(li);
 
-  return needingPlace.appendChild(li);
+  return needingPlace.insertAdjacentHTML("afterbegin", li);
 });
+// В этом случае подставляется url, alt только от первого <img>
+// needingPlace.insertAdjacentHTML(
+//   "afterbegin",
+//   '<li><img url="https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", alt="White and Black Long Fur Cat"</li>',
+//   '<li><img url="https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", alt="Orange and White Koi Fish Near Yellow Koi Fish"</li>',
+//   '<li><img url="https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", alt="Group of Horses Running"</li>'
+// );
